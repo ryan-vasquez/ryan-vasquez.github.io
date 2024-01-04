db40dc94397fc2e02daf3b8fd91d035c0f0f2ca9
ryan-vasquez <ryavasquez@gmail.com>
2024-01-03 20:44:53 -0700

    Added picture of us and use countdown

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ function answerQuestion() {
     document.getElementById('question-container').style.visibility="hidden";
     document.getElementById('answer-container').style.visibility="visible";
 }
+
+var countDownDate = new Date("Jan 10, 2024 17:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "I'M HEREEEEE";
+  }
+}, 1000);
